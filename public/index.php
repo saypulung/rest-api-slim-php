782d2977de022b187481eb2fbcd94b400d258af2
Say Pulung <saypulung@gmail.com>
2024-11-16 16:36:26 +0800

    Add cors, fix uncompatible in MacOS

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 require __DIR__ . '/../src/App/App.php';
 
 $app->run();
--- src/App/Routes.php
+++ src/App/Routes.php
@@ -8,6 +8,17 @@ use App\Controller\User;
 use App\Middleware\Auth;
 
 return static function ($app) {
+   $app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
     $app->get('/', 'App\Controller\DefaultController:getHelp');
     $app->get('/status', 'App\Controller\DefaultController:getStatus');
     $app->post('/login', \App\Controller\User\Login::class);
